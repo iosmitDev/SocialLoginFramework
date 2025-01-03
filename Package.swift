04c51f1d57e091ffcabb73916eb9ac89c0de178e
iosmitDev <mitesh.imply@gmail.com>
2025-01-03 17:19:19 +0530

    [Feature] Adding New framework code

--- Package.swift
+++ Package.swift
@@ -5,19 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "SocialLogin",
+    platforms: [ .iOS(.v15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SocialLogin",
             targets: ["SocialLogin"]),
+    ],
+    dependencies: [
+        //Put here dependency like sdwebimage
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SocialLogin"),
-        .testTarget(
-            name: "SocialLoginTests",
-            dependencies: ["SocialLogin"]),
+            name: "SocialLogin",
+            dependencies: [])
     ]
 )
--- Sources/SocialLogin/SocialLogin.swift
+++ Sources/SocialLogin/SocialLogin.swift
@@ -1,2 +1,24 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+//Create ViewModifier for direct use in app
+
+@available(macOS 10.15, *)
+extension View {
+    
+    //Center horizontal => means in HStack => left space , right space and between content self
+    //ByDefault internal so make it public for use outside
+    public func centerHorizontaly() -> some View {
+        
+        HStack {
+            
+            Spacer()
+            
+            self
+            
+            Spacer()
+        }
+    }
+}
